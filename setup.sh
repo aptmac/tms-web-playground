9945b31448f76cdebbd4b5e1e4bb378d86d0324d
Andrew Azores <me@andrewazor.es>
2017-03-15 12:44:14 -0400

    Setup script checks for global grunt and bower before proceeding

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
 # installs the dependencies, generates & launches the application
 
-npm install;
-npm install bower;
-bower install;
-npm install grunt-cli;
-grunt server;
+function require() {
+    command -v "$1" >/dev/null 2>&1 || { echo >&2 "'$1' executable is required globally but not found"; exit 1; }
+}
+
+require grunt
+require bower
+
+npm install
+bower install
+npm install --save-dev grunt
+grunt server

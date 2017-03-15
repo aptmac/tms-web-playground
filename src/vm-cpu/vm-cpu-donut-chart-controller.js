730c3f0a6b33db2b8c46b759b823076bf32c7074
Andrew Azores <me@andrewazor.es>
2017-03-15 13:38:10 -0400

    Extract chart generation logic to Angular controllers

---
+++ src/vm-cpu/vm-cpu-donut-chart-controller.js
@@ -0,0 +1,32 @@
+angular.module('apf.vmCpuModule').controller('vmCpuDonutChartController',[function () {
+  'use strict';
+  var donutConfig = $().c3ChartDefaults().getDefaultDonutConfig('A');
+  donutConfig.bindto = '#chart-pf-donut-1';
+  donutConfig.color =  {
+    pattern: ["#cc0000","#D1D1D1"]
+  };
+  donutConfig.data = {
+    type: "donut",
+    columns: [
+      ["Used", 95],
+      ["Available", 5]
+    ],
+    groups: [
+      ["used", "available"]
+    ],
+    order: null
+  };
+  donutConfig.tooltip = {
+    contents: function (d) {
+      return '<span class="donut-tooltip-pf" style="white-space: nowrap;">' +
+        Math.round(d[0].ratio * 100) + '%' + ' MHz ' + d[0].name +
+        '</span>';
+    }
+  };
+
+  var chart1 = c3.generate(donutConfig);
+  var donutChartTitle = d3.select("#chart-pf-donut-1").select('text.c3-chart-arcs-title');
+  donutChartTitle.text("");
+  donutChartTitle.insert('tspan').text("950").classed('donut-title-big-pf', true).attr('dy', 0).attr('x', 0);
+  donutChartTitle.insert('tspan').text("MHz Used").classed('donut-title-small-pf', true).attr('dy', 20).attr('x', 0);
+}]);
